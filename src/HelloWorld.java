a127c5a1e80d122220db02dc6c2bf8a606c73957
William Makowski <wfm692@hotmail.com>
2020-02-28 08:07:07 -0500

    Had a few mistakes to correct in first run

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
-Public class HelloWorld {
-    public static void public static void main(String[] args) {
+public class HelloWorld {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
